57760b67d7f392f45cc388c628db3621c02de718
Ayush Kumar Singh <ayushkumarsingh06@gmail.com>
2020-09-28 22:05:50 +0530

    server side code trial

--- calculatorfunctions.js
+++ calculatorfunctions.js
@@ -1,62 +1,62 @@
 document.getElementById("1").addEventListener("click", function(){
-    one("1");
+    equation("1");
 })
 
 document.getElementById("2").addEventListener("click", function(){
-    one("2");
+    equation("2");
 })
 
 document.getElementById("3").addEventListener("click", function(){
-    one("3");
+    equation("3");
 })
 
 document.getElementById("4").addEventListener("click", function(){
-    one("4");
+    equation("4");
 })
 
 document.getElementById("5").addEventListener("click", function(){
-    one("5");
+    equation("5");
 })
 
 document.getElementById("6").addEventListener("click", function(){
-    one("6");
+    equation("6");
 })
 
 document.getElementById("7").addEventListener("click", function(){
-    one("7");
+    equation("7");
 })
 
 document.getElementById("8").addEventListener("click", function(){
-    one("8");
+    equation("8");
 })
 
 document.getElementById("9").addEventListener("click", function(){
-    one("9");
+    equation("9");
 })
 
 
 document.getElementById("0").addEventListener("click", function(){
-    one("0");
+    equation("0");
 })
 
 document.getElementById("plus").addEventListener("click", function(){
-    one("+");
+    equation("+");
 })
 
 document.getElementById("minus").addEventListener("click", function(){
-    one("-");
+    equation("-");
 })
 
 document.getElementById("mult").addEventListener("click", function(){
-    one("*");
+    equation("*");
 })
 
 document.getElementById("divi").addEventListener("click", function(){
-    one("/");
+    equation("/");
 })
 
 document.getElementById("dot").addEventListener("click", function(){
-    one(".");
+    equation(".");
 })
 
 document.getElementById("equal").addEventListener("click", function(){
@@ -67,7 +67,11 @@ document.getElementById("clear").addEventListener("click", function(){
     document.getElementById("equationinput").value = "";
 })
 
-function one(number){
+document.getElementById("history").addEventListener("click", function(){
+    getHistoryCookie();
+})
+
+function equation(number){
     document.getElementById("equationinput").value += number;
     console.log(document.getElementById("equationinput").value);
 }
@@ -75,6 +79,26 @@ function one(number){
 function evaluate(value){
     let answer = eval(value);
     document.getElementById("equationinput").value = answer;
-    console.log(eval(value));
+    let historyvalue = value+" = "+answer;
+    console.log(historyvalue);
+    setHistoryCookie(historyvalue);
 }
 
+// function for setting cookie, taking equation from user and getting the timestamp in the function. 
+// timestamp will be our key and equation will be our value
+function setHistoryCookie(equation){
+    let datetime =  new Date().toLocaleString();
+    document.cookie = datetime + "=" + equation;
+    // document.cookie = "ayush=kumar";
+}
+
+// get cookie
+function getHistoryCookie(){
+    console.log("into history");
+    document.cookie = "ayush=kumar";
+    console.log(document.cookie);
+    // let history = document.cookie.split(';');
+    // for(i = 0; i<history.length; i++){
+    //     console.log(history[i]);
+    // }
+}
